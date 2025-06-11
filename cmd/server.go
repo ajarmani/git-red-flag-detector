daff344019643071f6afe95848a0d14f32490aff
ajarmani <mahavishnu.balakrishnan@gmail.com>
2025-06-11 22:19:53 +0530

    added scanner with red flag regex

--- cmd/server.go
+++ cmd/server.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	"github.com/ajarmani/git-red-flag-detector/internal/resolver"
+	"github.com/ajarmani/git-red-flag-detector/internal/scanner"
 )
 
 func main() {
-	repoPath := "insert/local/path/to/your/repo"
-	commitHash := "insert_commit_hash_here"
+	// For testing purposes, using hello-world repo
+	repoPath := "/Users/mbalakrishna/Desktop/Desktop/Golang/hello-world"
+	commitHash := "2215cb0"
 
 	diffs, err := resolver.GetCommitDiff(repoPath, commitHash)
 	if err != nil {
@@ -17,7 +19,9 @@ func main() {
 	}
 
 	for _, d := range diffs {
-		fmt.Printf("File: %s\n", d.FileName)
-		fmt.Println(d.Diff)
+		flags := scanner.ScanDiff(d.FileName, d.Diff)
+		for _, flag := range flags {
+			fmt.Printf("RED FLAG in %s:\n   ➤ Pattern: %s\n   ➤ Line: %s\n\n", flag.FileName, flag.Pattern, flag.Line)
+		}
 	}
 }
