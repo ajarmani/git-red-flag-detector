ce00eea4ff5d57e53da41af87b98982789e20284
ajarmani <mahavishnu.balakrishnan@gmail.com>
2025-06-14 09:47:19 +0530

    Added n commit scan option from cli

--- cmd/server.go
+++ cmd/server.go
@@ -12,23 +12,49 @@ import (
 func main() {
 	repoPath := flag.String("repo", "", "Absolute path to the Git repo")
 	commitHash := flag.String("commit", "", "Commit hash to scan")
+	lastN := flag.Int("last", 0, "Number of latest commits to scan")
+
 	flag.Parse()
 
-	if *repoPath == "" || *commitHash == "" {
-		fmt.Println("Usage: go run cmd/server.go --repo /path/to/repo --commit <hash>")
+	if *repoPath == "" || (*commitHash == "" && *lastN == 0) {
+		fmt.Println("Usage: go run cmd/server.go --repo /path/to/repo --commit <hash> OR --last <n>")
 		os.Exit(1)
 	}
 
-	diffs, err := resolver.GetCommitDiff(*repoPath, *commitHash)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+	if *lastN > 0 {
+		commits, err := resolver.GetLastNCommits(*repoPath, *lastN)
+		if err != nil {
+			fmt.Println("Error fetching commits:", err)
+			os.Exit(1)
+		}
 
-	for _, d := range diffs {
-		flags := scanner.ScanDiff(d.FileName, d.Diff)
-		for _, flag := range flags {
-			fmt.Printf("RED FLAG in %s:\n   ➤ Pattern: %s\n   ➤ Line: %s\n\n", flag.FileName, flag.Pattern, flag.Line)
+		for _, hash := range commits {
+			fmt.Printf("\nScanning commit: %s\n", hash)
+			diffs, err := resolver.GetCommitDiff(*repoPath, hash)
+			if err != nil {
+				fmt.Printf("Error scanning %s: %v\n", hash, err)
+				continue
+			}
+
+			for _, d := range diffs {
+				flags := scanner.ScanDiff(d.FileName, d.Diff)
+				for _, flag := range flags {
+					fmt.Printf("RED FLAG in %s:\n   ➤ Pattern: %s\n   ➤ Line: %s\n\n", flag.FileName, flag.Pattern, flag.Line)
+				}
+			}
+		}
+	} else {
+		diffs, err := resolver.GetCommitDiff(*repoPath, *commitHash)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		for _, d := range diffs {
+			flags := scanner.ScanDiff(d.FileName, d.Diff)
+			for _, flag := range flags {
+				fmt.Printf("RED FLAG in %s:\n   ➤ Pattern: %s\n   ➤ Line: %s\n\n", flag.FileName, flag.Pattern, flag.Line)
+			}
 		}
 	}
 }
